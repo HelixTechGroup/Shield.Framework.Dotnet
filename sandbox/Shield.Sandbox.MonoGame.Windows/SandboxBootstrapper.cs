cc2a733985b47c71239ab4c80ba0912e96b3339e
blongacre <blongacre@empiregamingllc.com>
2018-04-11 12:33:48 -0400

    Moved sandbox directory to project root.
    Added Shield.Framework.Core.Tests
    Added GlobalSuppressions

---
+++ sandbox/Shield.Sandbox.MonoGame.Windows/SandboxBootstrapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Shield.Framework.Platform;
+
+namespace Shield.Sandbox.MonoGame.Windows
+{
+    public class SandboxBootstrapper : MonoGameWindowsBootstrapper
+    {
+        protected override void OnGameInitialize(object sender, EventArgs e)
+        {
+            base.OnGameInitialize(sender, e);
+        }
+
+        protected override void OnGameLoad(object sender, EventArgs e)
+        {
+            base.OnGameLoad(sender, e);
+        }
+
+        protected override void OnGameUnload(object sender, EventArgs e)
+        {
+            base.OnGameUnload(sender, e);
+        }
+
+        protected override void OnGameUpdate(object sender, GameTimeEventArgs e)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.OnGameUpdate(sender, e);
+        }
+
+        protected override void OnGameDraw(object sender, GameTimeEventArgs e)
+        {
+            m_game.GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.OnGameDraw(sender, e);
+        }                
+    }
+}
---
+++ src/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Sonar Code Smell", "S2292:Trivial properties should be auto-implemented")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Sonar Code Smell", "S1172:Unused method parameters should be removed")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Sonar Code Smell", "S4035:Classes implementing \"IEquatable<T>\" should be sealed")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0028:Simplify collection initialization")]
